9d7d441cfe98a4be1551e1bddd5164dd2f3b2999
Uzair Sajid <uzairsajidgit512@gmail.com>
2025-08-18 13:34:20 +0500

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,9 @@
+/**type animation */
+
+var type = new Typed(".typing",{
+    strings:["","Web Designer","Web Developer","Graphic Designer","Ui/Ux Designer"],
+    typeSpeed:100,
+    backSpeed:60,
+    loop:true   
+
+})
