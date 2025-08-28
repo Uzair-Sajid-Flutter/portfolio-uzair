3986788aade55bad6eff9d8aaeacfb6692bf5199
Uzair Sajid <uzairsajidgit512@gmail.com>
2025-08-28 14:33:42 +0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 /**type animation */
 
 var type = new Typed(".typing",{
-    strings:["","Web Designer","Web Developer","Graphic Designer","Ui/Ux Designer"],
+    strings:["","Web Designer","Web Developer","Graphic Designer","Ui/Ux Designer","Freelancer"],
     typeSpeed:100,
     backSpeed:60,
     loop:true   
